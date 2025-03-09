a002bc5fa2f212a7fc6a38384ce8d3182f903155
soumya motwani <motwanisoumya20gmail.com>
2025-03-09 14:08:12 +0530

    refurbished captain regis

--- backend/middlewares/auth.middleware.js
+++ backend/middlewares/auth.middleware.js
@@ -1,6 +1,8 @@
 const userModel = require('../models/user.model');
 const brcypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const blacklistedTokenModel = require('../models/blacklistToken.model');    
+const captainModel = require('../models/captain.model');
 
 
 
@@ -29,4 +31,32 @@ module.exports.authUser = async(req,res,next) => {
         console.error('Token verification error:', err.message);
         return res.status(401).json({message: 'Unauthorized: Invalid token'});
     }
+}
+module.exports.authCaptain = async(req,res,next) => { 
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+    console.log(token);
+    if (!token) {
+        return res.status(401).json({message: 'Unauthorized'});
+    } 
+    const blacklistedToken = await userModel.findOne({token: token
+    });
+    console.log(blacklistedToken);
+    if (blacklistedToken) {
+        return res.status(401).json({message: 'Unauthorized: Token blacklisted'});
+    }     
+    try { 
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id);
+        if (!captain) {
+            return res.status(401).json({ message: 'Unauthorized: Captain not found' });
+        }
+         
+        req.captain = captain;
+
+        return next();
+    } 
+    catch (err) {
+       console.log(err);
+        return res.status(401).json({message: 'Unauthorized: Invalid token'});
+    }   
 }
